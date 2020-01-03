45feec59609d066c6485274cb7c368aae0350076
Hari <>
2020-01-03 10:43:50 +0530

    Add project files.

---
+++ XamarinForms/XamarinForms/LogInViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace XamarinForms
+{
+    public class LogInViewModel : INotifyPropertyChanged
+    {
+        public Action DisplayInvalidLoginPrompt;
+        public Action DisplaySuccessfullyLoggedInPromt;
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+        public ICommand SubmitCommand { protected set; get; }
+        public LogInViewModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+        public void OnSubmit()
+        {
+            if (email != "admin" || password != "1234")
+            {
+                DisplayInvalidLoginPrompt();
+            } else
+            {
+                DisplaySuccessfullyLoggedInPromt();
+            }
+        }
+    }
+}
---
+++ XamarinForms/XamarinForms/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var viewModel = new LogInViewModel();
+            this.BindingContext = viewModel;
+            viewModel.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Invalid Login, try again", "OK");
+            viewModel.DisplaySuccessfullyLoggedInPromt += () => DisplayAlert("Success", "You are logged in Successfully", "OK");
+            
+            InitializeComponent();
+
+            Email.Completed += (object sender, EventArgs e) =>
+            {
+                Password.Focus();
+            };
+
+            Password.Completed += (object sender, EventArgs e) =>
+            {
+                viewModel.SubmitCommand.Execute(null);
+            };
+        }
+    }
+}
